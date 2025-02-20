4ca8a7603f39d267abcfb67709b6818e7793b698
LAPTOP-Q462S81B\ASUS <doantrungminh20@gmail.com>
2025-02-20 16:52:04 +0700

    Master

---
+++ src/modules/users/schemas/user.schema.ts
@@ -0,0 +1,44 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { IsEmpty } from 'class-validator';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema({ timestamps: true })
+export class User {
+  @IsEmpty()
+    name: string;
+
+    @Prop()
+    email: string;
+
+    @Prop()
+    password: string;
+
+    @Prop()
+    phone: string;
+
+    @Prop()
+    address: string;
+
+    @Prop()
+    image: string;
+
+  @Prop({ default: 'USERS' })
+    role: string;
+
+  @Prop({ default: 'USERS' })
+    accountType: string;
+
+  @Prop({ default: false })
+    isActive: boolean;
+
+    @Prop()
+    codeId: string;
+
+    @Prop()
+    codeExpired: Date;
+
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -6,9 +14,10 @@ import { UpdateUserDto } from './dto/update-user.dto';
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-
+  
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
+    console.log('>> check createUserDto: ', createUserDto);
     return this.usersService.create(createUserDto);
   }
 
